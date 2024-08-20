2615ff73104b967f5b2c295e931217756423c849
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-08-20 10:51:42 +0800

    优化readme

--- src/utils/http.js
+++ src/utils/http.js
@@ -7,6 +7,7 @@ import {useCaptchaStore} from "@/stores/captcha.js";
 // 创建axios实例
 const http = axios.create({
   baseURL: 'http://120.26.203.121:8888',
+  // baseURL: 'http://127.0.0.1:6677',
   timeout: 2000
 })
 
