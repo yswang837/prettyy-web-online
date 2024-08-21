67da39764b4a7a530388c583cc1b80544e9c9bf9
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-08-21 19:17:09 +0800

    在axios请求拦截器中，post请求统一添加caller和sign

--- src/utils/http.js
+++ src/utils/http.js
@@ -7,8 +7,8 @@ import makeSign from "@/utils/makeSign.js";
 
 // 创建axios实例
 const http = axios.create({
-  baseURL: 'http://120.26.203.121:8888',
-  // baseURL: 'http://127.0.0.1:6677',
+  // baseURL: 'http://120.26.203.121:8888',
+  baseURL: 'http://127.0.0.1:6677',
   timeout: 2000
 })
 
@@ -24,6 +24,11 @@ http.interceptors.request.use(config => {
   if (config.method === 'get') {
     config.params = Object.assign({}, config.params, { caller: 'web' })
     config.params = Object.assign({}, config.params, {sign: makeSign(config.params)})
+  }
+  if (config.method === 'post') {
+    config.headers["Content-Type"] = 'application/x-www-form-urlencoded'
+    config.data = Object.assign({}, config.data, { caller: 'web' })
+    config.data = Object.assign({}, config.data, {sign: makeSign(config.data)})
   }
   return config
 }, e => Promise.reject(e))
