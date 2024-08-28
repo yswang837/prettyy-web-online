ba90a46ffee05230e3224c3950a0fd45e91295bb
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-08-28 10:16:45 +0800

    fix makeSign函数的逻辑错误导致file字段也参与了验签

--- src/utils/makeSign.js
+++ src/utils/makeSign.js
@@ -5,7 +5,8 @@ export default function makeSign(params) {
     const pin = "4d3abacc23255a0767c718afda30cc8c"
     let stringToSign = ''
     Object.keys(params).sort().forEach(key => {
-        if (key !== 'sign' || key !== 'file' || key !== 'content'){
+        if (key !== 'sign' && key !== 'file' && key !== 'content'){
+            // console.log('key',key,params[key])
             stringToSign += `${key}=${params[key]}`
         }
     });
