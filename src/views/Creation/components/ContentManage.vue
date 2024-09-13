ff01494c3b4d382979df95d0ce7be17379e942a0
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-09-13 15:11:41 +0800

    创作中心-内容管理 基本静态页面渲染

--- src/views/Creation/components/ContentManage.vue
+++ src/views/Creation/components/ContentManage.vue
@@ -22,18 +22,45 @@ onMounted(() => {getArticleList(getUidFromJwt(), 1,20)})
 
 <template>
 <div>
-  <el-tabs v-model="activeName" @tab-click="handleClick">
-    <el-tab-pane label="文章" name="tab1">
-      <ArticlePanel v-for="item in articleList" :key="item.aid" :aid="item.aid" :title="item.title" :cover-img="item.cover_img" :is-show-edit="true" :is-show-delete="true"/>
-    </el-tab-pane>
-    <el-tab-pane label="下载" name="tab2">下载</el-tab-pane>
-    <el-tab-pane label="问答" name="tab3">问答</el-tab-pane>
-    <el-tab-pane label="视频" name="tab4">视频</el-tab-pane>
-  </el-tabs>
-
+  <div class="content-container">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="文章" name="tab1">
+        <ul class="sub-tabs">
+          <li>全部(100)</li>
+          <li>全部可见(89)</li>
+          <li>Vip可见(22)</li>
+          <li>粉丝可见(48)</li>
+          <li>仅我可见(2)</li>
+          <li>原创(33)</li>
+          <li>转载(47)</li>
+          <li>翻译(11)</li>
+          <li>草稿箱(23)</li>
+          <li>回收站(38)</li>
+        </ul>
+        <ArticlePanel v-for="item in articleList" :key="item.aid" :aid="item.aid" :title="item.title" :cover-img="item.cover_img" :is-show-edit="true" :is-show-delete="true"/>
+      </el-tab-pane>
+      <el-tab-pane label="下载" name="tab2">下载</el-tab-pane>
+      <el-tab-pane label="问答" name="tab3">问答</el-tab-pane>
+      <el-tab-pane label="视频" name="tab4">视频</el-tab-pane>
+    </el-tabs>
+  </div>
 </div>
 </template>
 
 <style scoped lang="scss">
+.content-container {
+  padding: 10px 30px;
+}
+.sub-tabs {
+  display: flex;
+  margin-bottom: 10px;
+  color: #b0b0b0;
+  font-size: 13px;
+
+}
+.sub-tabs li {
+  margin-right: 10px;
+}
+
 
 </style>
