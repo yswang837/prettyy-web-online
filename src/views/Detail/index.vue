92967419fea6cbccbfa2cd83b9705ec7fc000ecd
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-09-30 14:09:54 +0800

    文章详情页，文章列表区域

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -51,38 +51,40 @@ onMounted(() => {
           <ArticleList article-type="hot"/>
           <ArticleList article-type="new"/>
         </div>
-        <div class="article-content">
-          <div class="article-content-title">
-            {{articleDetail?.title}}
-          </div>
-          <div class="sample-desc">
-            <img class="article-type" src="@/assets/images/original.png" alt="">
-            <div class="detail-info">
-              <div>
-                <span class="username">{{articleUserStore.articleUserInfo.nick_name}}</span>
-                <i v-if="articleDetail?.create_time===articleDetail?.update_time" class="iconfont icon-shizhongfill"><span class="detail-info-item">发布于{{articleDetail?.create_time}}</span></i>
-                <i v-if="articleDetail?.create_time!==articleDetail?.update_time" class="iconfont icon-shizhongfill"><span class="detail-info-item">修改于{{articleDetail?.update_time}}</span></i>
-                <i class="iconfont icon-yuedushu"><span class="detail-info-item">阅读量{{articleDetail?.read_num}}</span></i>
-                <i class="iconfont icon-shoucangshu-yishoucang"><span class="detail-info-item">收藏{{articleDetail?.collect_num}}</span></i>
-                <i class="iconfont icon-dianzan_kuai"><span class="detail-info-item">点赞数{{articleDetail?.like_num}}</span></i>
-              </div>
-              <div class="article-tags">
-                文章标签
-                <el-tag class="tag" v-for="(tag,index) in tags" :key="index"  :disable-transitions="false">{{tag}}</el-tag>
-              </div>
+        <div class="article-content-container">
+          <div class="article-content">
+            <div class="article-content-title">
+              {{articleDetail?.title}}
             </div>
-            <div class="copy-right" @click="showCopyRight">版权</div>
-          </div>
-          <div v-if="isShowCopyRight" class="copy-right-detail">
-            <div>版权声明：本文为博主原创文章，遵循 <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">CC 4.0 BY-SA</a> 版权协议，转载请附上原文出处链接和本声明。</div>
-            <div style="margin-top: 5px">本文链接：http://120.26.203.121{{fullPath}}</div>
-          </div>
-          <div class="article-detail" v-html="articleDetail?.content"></div>
-          <div class="knowledge-tree">
-            知识树
-          </div>
-          <div class="article-interactive">
-            文章交互区域
+            <div class="sample-desc">
+              <img class="article-type" src="@/assets/images/original.png" alt="">
+              <div class="detail-info">
+                <div>
+                  <span class="username">{{articleUserStore.articleUserInfo.nick_name}}</span>
+                  <i v-if="articleDetail?.create_time===articleDetail?.update_time" class="iconfont icon-shizhongfill"><span class="detail-info-item">发布于{{articleDetail?.create_time}}</span></i>
+                  <i v-if="articleDetail?.create_time!==articleDetail?.update_time" class="iconfont icon-shizhongfill"><span class="detail-info-item">修改于{{articleDetail?.update_time}}</span></i>
+                  <i class="iconfont icon-yuedushu"><span class="detail-info-item">阅读量{{articleDetail?.read_num}}</span></i>
+                  <i class="iconfont icon-shoucangshu-yishoucang"><span class="detail-info-item">收藏{{articleDetail?.collect_num}}</span></i>
+                  <i class="iconfont icon-dianzan_kuai"><span class="detail-info-item">点赞数{{articleDetail?.like_num}}</span></i>
+                </div>
+                <div class="article-tags">
+                  文章标签
+                  <el-tag class="tag" v-for="(tag,index) in tags" :key="index"  :disable-transitions="false">{{tag}}</el-tag>
+                </div>
+              </div>
+              <div class="copy-right" @click="showCopyRight">版权</div>
+            </div>
+            <div v-if="isShowCopyRight" class="copy-right-detail">
+              <div>版权声明：本文为博主原创文章，遵循 <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">CC 4.0 BY-SA</a> 版权协议，转载请附上原文出处链接和本声明。</div>
+              <div style="margin-top: 5px">本文链接：http://120.26.203.121{{fullPath}}</div>
+            </div>
+            <div class="article-detail" v-html="articleDetail?.content"></div>
+            <div class="knowledge-tree">
+              知识树
+            </div>
+            <div class="article-interactive">
+              文章交互区域
+            </div>
           </div>
           <div class="detail-article-list">
             文章详情页，文章列表区域
@@ -113,75 +115,79 @@ onMounted(() => {
       background-color: white;
     }
   }
-  .article-content {
+  .article-content-container {
     flex: 2.90;
     margin: 0 10px;
-    padding: 20px 30px;
-    background-color: white;
-    .article-content-title {
-      font-size: 27px;
-    }
-    .sample-desc {
-      display: flex;
-      justify-content: space-between;
-      background-color: #f8f8f8;
-      width: 100%;
-      height: 66px;
-      padding: 10px;
-      .article-type {
-        width: 36px;
-        height: 32px;
+    .article-content {
+      background-color: white;
+      padding: 20px 30px;
+      .article-content-title {
+        font-size: 27px;
       }
-      .detail-info {
-        .username {
-          margin-right: 20px;
-          color: #555665;
-          font-size: 14px;
+      .sample-desc {
+        display: flex;
+        justify-content: space-between;
+        background-color: #f8f8f8;
+        width: 100%;
+        height: 66px;
+        padding: 10px;
+        .article-type {
+          width: 36px;
+          height: 32px;
         }
-        .icon-shizhongfill,.icon-yuedushu, .icon-shoucangshu-yishoucang, .icon-dianzan_kuai {
-          margin-right: 20px;
-          color: #999aa9;
-          font-size: 14px;
-        }
-        .article-tags {
-          margin: 8px 0 3px 0;
-          color: #999aa9;
-          .tag {
-            margin: -3px 8px 0 8px;
-            cursor: pointer;
+        .detail-info {
+          .username {
+            margin-right: 20px;
+            color: #555665;
+            font-size: 14px;
+          }
+          .icon-shizhongfill,.icon-yuedushu, .icon-shoucangshu-yishoucang, .icon-dianzan_kuai {
+            margin-right: 20px;
+            color: #999aa9;
+            font-size: 14px;
+          }
+          .article-tags {
+            margin: 8px 0 3px 0;
+            color: #999aa9;
+            .tag {
+              margin: -3px 8px 0 8px;
+              cursor: pointer;
+            }
+          }
+          .detail-info-item {
+            margin-left: 4px;
           }
         }
-        .detail-info-item {
-          margin-left: 4px;
+        .copy-right {
+          color: #9eb5ca;
+          cursor: pointer;
         }
       }
-      .copy-right {
-        color: #9eb5ca;
-        cursor: pointer;
+      .copy-right-detail {
+        margin-top: 20px;
+        color: #9a9aab;
       }
-    }
-    .copy-right-detail {
-      margin-top: 20px;
-      color: #9a9aab;
-    }
-    .article-detail {
-      margin: 40px 0;
-    }
-    .knowledge-tree {
-      width: 100%;
-      height: 100px;
-      border-top: 1px solid #f0f0f2;
-      border-bottom: 1px solid #f0f0f2;
-    }
-    .article-interactive {
-      position: sticky;
-      bottom: 0;
-      background-color: #cccccc;
-      width: 100%;
-      height: 65px;
+      .article-detail {
+        margin: 40px 0;
+      }
+      .knowledge-tree {
+        width: 100%;
+        height: 100px;
+        border-top: 1px solid #f0f0f2;
+        border-bottom: 1px solid #f0f0f2;
+      }
+      .article-interactive {
+        position: sticky;
+        bottom: 0;
+        background-color: #cccccc;
+        width: 100%;
+        height: 65px;
+      }
     }
     .detail-article-list {
       height: 800px;
+      margin: 10px 0;
+      background-color: white;
     }
   }
   .article-right {
