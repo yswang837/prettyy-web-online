474a236dc7c25f1aac7ccf30f2c1364b7b975064
小钻风 <80513574+yswang837@users.noreply.github.com>
2024-10-15 11:44:07 +0800

    点赞测试

--- src/views/Message/index.vue
+++ src/views/Message/index.vue
@@ -1,11 +1,51 @@
-<script setup>
-
-</script>
-
 <template>
-<div>消息中心</div>
+  <div>
+    <button @click="sendLike">点赞</button>
+    <div v-for="msg in messages" :key="msg">{{ msg }}</div>
+  </div>
 </template>
 
-<style scoped lang="scss">
-
-</style>
+<script>
+export default {
+  data() {
+    return {
+      websocket: null,
+      messages: []
+    };
+  },
+  created() {
+    this.setupWebSocket();
+  },
+  beforeUnmount() {
+    this.closeWebSocket();
+  },
+  methods: {
+    setupWebSocket() {
+      this.websocket = new WebSocket("ws://localhost:8888/websocket/connection");
+      this.websocket.onopen = this.onWebSocketOpen;
+      this.websocket.onmessage = this.onWebSocketMessage;
+      this.websocket.onclose = this.onWebSocketClose;
+    },
+    onWebSocketOpen() {
+      console.log("WebSocket连接成功");
+    },
+    onWebSocketMessage(event) {
+      this.messages.push(event.data);
+    },
+    onWebSocketClose() {
+      console.log("WebSocket连接关闭");
+    },
+    closeWebSocket() {
+      if (this.websocket) {
+        this.websocket.close();
+      }
+    },
+    sendLike() {
+      fetch("http://120.26.203.121:8888/like", {
+        method: "POST"
+      }).then(response => response.json())
+          .then(data => console.log(data));
+    }
+  }
+};
+</script>
